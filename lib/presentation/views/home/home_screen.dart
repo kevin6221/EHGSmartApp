3d4b3595c3731bad466e337e76ef1562ff95a9c8
kevinCt006 <kevin.vachhani@mail.codetrade.io>
2026-09-16 19:09:20 +0530

    style: refine Home screen layout spacing
    - Add vertical spacing after the activity summary section in `HomeScreen` to improve layout consistency.

--- lib/presentation/views/home/home_screen.dart
+++ lib/presentation/views/home/home_screen.dart
@@ -148,6 +148,7 @@ class _HomeScreenState extends State<HomeScreen>
                         goalMins: data.goalMins,
                         weeklyEnergy: data.weeklyEnergy,
                       ),
+                      SizedBox(height: itemSpacing),
                     ],
                   ),
                 ),
